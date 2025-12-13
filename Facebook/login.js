c3f9043f1d6133b097cea022b3ce96bbce878d98
MrAsh-007 <mrash2006@gmail.com>
2025-12-13 17:45:50 +0500

    Working on login.js

--- Facebook/login.js
+++ Facebook/login.js
@@ -1,14 +1,21 @@
 // Login Page
 
-function logInCredintials() {
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+function logInCredintials(e) {
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
 
-    if (email == "" || password == "") {
-        alert(`Credintials cant be empty`);
+    e.preventDefault();
+
+    if (email.value.trim() == "" || password.value.trim() == "") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
     } else {
-        console.log(`Email ${email}`);
-        console.log(`Password ${password}`);
+        console.log(`Email ${email.value}`);
+        console.log(`Password ${password.value}`);
     }
 }
 
