a7cbec88130356bb0add0bf91a4b968ba920e29e
MrAsh-007 <mrash2006@gmail.com>
2026-01-28 18:57:06 +0500

    working on JS

--- Facebook/signup.js
+++ Facebook/signup.js
@@ -1,33 +1,69 @@
 // Signup Page
 
 
-function signUpCredintials(){
-    let firstName = document.getElementById("firstName").value;
-    let lastName = document.getElementById("lastName").value;
-
-    let day = document.getElementById("day").value;
-    let month = document.getElementById("month").value;
-    let year = document.getElementById("year").value;
-
-    let gender = document.querySelector('input[name="gender"]:checked')?.id;
-
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
 
 
+let signupbutton = document.getElementById("signupbutton");
+
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+
+let day = document.getElementById("day");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+
+let gender = document.getElementsByName("gender");
+
+
+let email = document.getElementById("email");
+let password = document.getElementById("password");
 
 
 
 
 
 
-    console.log(`firstname ${firstName}`);
-    console.log(`lastname ${lastName}`);
+function signUpCredintials() {
 
-    console.log(`DOB ${day} ${month} ${year}`);
+    
 
-    console.log(`Gender ${gender}`);
+    
+    let userObj = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        day: day.value,
+        month: month.value,
+        year: year.value,
+        email: email.value,
+        password: password.value,
+    };
+    
 
-    console.log(`email ${email}`);
-    console.log(`password ${password}`);
+
+    for (let i = 0; i < gender.length; i++) {
+        if (gender[i].checked) {
+            userObj.gender = gender[i].value;
+        }
+    }
+
+
+
+
+
+    let dbData = JSON.parse(localStorage.getItem("users"));
+    
+
+    dbData.push(userObj);
+    
+    
+
+    
+    localStorage.setItem("users", JSON.stringify(dbData));
+    
+    
+    
 }
+
+
+
+signupbutton.addEventListener("click", signUpCredintials);
